c044efb4367cf298781e402f34251d413b778e5e
Mwandoe-Shali <mwandoeshali@gmail.com>
2024-04-29 06:45:58 +0300

    Update Nginx configuration to include redirect and error pages

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,54 +1,50 @@
 # setup web server for deployment
 
-file { '/etc/nginx/sites-available/default':
-  ensure => present,
-  content => "
-    server {
-        listen 80 default_server;
-        listen [::]:80 default_server;        
-
-        root /var/www/html;
-
-        index index.html index.htm index.nginx-debian.html;
-
-        server_name _;
-        add_header X-Served-By ${hostname};
-
-        location /hbnb_static {
-            alias /data/web_static/current/;
-        }
-
-        location /redirect_me {
-            return 301 http://github.com/Mwandoe-Shali/;
-        }
-
-        error_page 404 /404.html
-        location /404 {
-            root /var/www/html:
-            internal;
-        }
+# Nginx configuration file
+$nginx_conf = "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By ${hostname};
+    root   /var/www/html;
+    index  index.html index.htm;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
     }
-  ",
-  notify => Service['nginx'],
-}
-
-service { 'nginx':
-  ensure => running,
-  enable => true,
-  require => File['/etc/nginx/sites-available/default'],
-}
+    location /redirect_me {
+        return 301 http://github.com/Mwandoe-Shali/;
+    }
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}"
 
 package { 'nginx':
   ensure   => 'present',
   provider => 'apt'
 } ->
 
-$directories = ['/data', '/data/web_static', '/data/web_static/releases',
-                '/data/web_static/releases/test', '/data/web_static/shared']
+file { '/data':
+  ensure  => 'directory'
+} ->
 
-file { $directories:
-  ensure => 'directory',
-}
+file { '/data/web_static':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/releases':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/releases/test':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/shared':
+  ensure => 'directory'
+} ->
 
 file { '/data/web_static/releases/test/index.html':
   ensure  => 'present',
@@ -80,6 +76,11 @@ file { '/var/www/html/index.html':
 file { '/var/www/html/404.html':
   ensure  => 'present',
   content => "Ceci n'est pas une page\n"
+} ->
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'present',
+  content => $nginx_conf
 } ->
 
 exec { 'nginx restart':
